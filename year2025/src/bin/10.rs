87bf427bf7e890a467504e7def5e42f7132c6e3b
BB223 <79644542+BB223@users.noreply.github.com>
2025-12-14 16:08:53 +0100

    year 2025 slow day 10

--- year2025/src/bin/10.rs
+++ year2025/src/bin/10.rs
@@ -1,40 +1,61 @@
 use std::collections::{HashMap, VecDeque};
 
+use indicatif::ParallelProgressIterator;
 use itertools::Itertools;
-use rayon::iter::{ParallelBridge, ParallelIterator};
+use rayon::{
+    ThreadPoolBuilder,
+    iter::{ParallelBridge, ParallelIterator},
+};
 use regex::Regex;
 
 pub fn part_one(input: &str) -> Option<usize> {
     let lines = input.lines();
-    let mut solution = 0;
-    let first = Regex::new(r"\[[\.#]+]").unwrap();
-    let regex = Regex::new(r"\(\d(?:,\d)*\)*").unwrap();
 
-    for line in lines {
-        let result = first.find(line);
+    let lights_regex = Regex::new(r"\[[\.#]+\]").unwrap();
+    let button_regex = Regex::new(r"\(\d(?:,\d)*\)*").unwrap();
 
-        let mat = rem_first_and_last(result.unwrap().as_str());
+    let solution = lines
+        .clone()
+        .par_bridge()
+        .progress_count(lines.count() as u64)
+        .map(|line| {
+            let light_requirements: Vec<char> = lights_regex
+                .find(line)
+                .map(|matches| matches.as_str())
+                .map(remove_parentheses)
+                .unwrap()
+                .chars()
+                .collect();
 
-        let result = regex.captures_iter(line);
-        let buttons: Vec<&str> = result
-            .map(|c| c.get_match().as_str())
-            .map(rem_first_and_last)
-            .collect();
-
-        let min_presses = brute_force(mat, &buttons, 1);
-        solution += min_presses.unwrap();
-    }
+            let buttons: Vec<Button> = button_regex
+                .captures_iter(line)
+                .map(|captures| captures.get_match().as_str())
+                .map(remove_parentheses)
+                .map(|button_str| {
+                    button_str
+                        .chars()
+                        .flat_map(|char| char.to_digit(10))
+                        .map(|digit| digit as usize)
+                        .collect()
+                })
+                .collect();
+            (buttons, light_requirements)
+        })
+        .map(|(buttons, light_requirements)| brute_force(&buttons, &light_requirements, 0).unwrap())
+        .sum();
     Some(solution)
 }
 
-fn rem_first_and_last(value: &str) -> &str {
+type Button = Vec<usize>;
+
+fn remove_parentheses(value: &str) -> &str {
     let mut chars = value.chars();
     chars.next();
     chars.next_back();
     chars.as_str()
 }
 
-fn brute_force(solution: &str, buttons: &[&str], presses: usize) -> Option<usize> {
+fn brute_force(buttons: &[Button], light_requirements: &[char], presses: usize) -> Option<usize> {
     if presses == 10 {
         return None;
     }
@@ -43,22 +64,17 @@ fn brute_force(solution: &str, buttons: &[&str], presses: usize) -> Option<usize
         .iter()
         .combinations_with_replacement(presses)
         .any(|combination| {
-            let mut lights = HashMap::new();
+            let mut lights = vec![0; light_requirements.len()];
             for button in combination {
-                for c in button.chars() {
-                    if let Some(n) = lights.get(&c) {
-                        lights.insert(c, n + 1);
-                    } else {
-                        lights.insert(c, 1);
-                    }
+                for index in button {
+                    lights[*index] += 1;
                 }
             }
-            solution.chars().enumerate().all(|(i, c)| {
-                let indx = char::from_digit(i as u32, 10).unwrap();
-                if c == '.' {
-                    lights.get(&indx).unwrap_or(&0) % 2 == 0
+            light_requirements.iter().enumerate().all(|(i, c)| {
+                if *c == '.' {
+                    lights[i] % 2 == 0
                 } else {
-                    lights.get(&indx).unwrap_or(&0) % 2 == 1
+                    lights[i] % 2 == 1
                 }
             })
         })
@@ -66,157 +82,89 @@ fn brute_force(solution: &str, buttons: &[&str], presses: usize) -> Option<usize
         return Some(presses);
     }
 
-    brute_force(solution, buttons, presses + 1)
+    brute_force(buttons, light_requirements, presses + 1)
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
+    // example input
+    //
+    // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    // [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+    // [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+    //
+    let pool = ThreadPoolBuilder::new().num_threads(10).build().unwrap();
     let lines = input.lines();
-    let mut solution = 0;
-    let first = Regex::new(r"\{\d+(?:,\d+)*\}").unwrap();
-    let regex = Regex::new(r"\(\d(?:,\d)*\)*").unwrap();
 
-    for line in lines {
-        let result = first.find(line);
-        let joltage_requirements = rem_first_and_last(result.unwrap().as_str());
+    let joltage_regex = Regex::new(r"\{\d+(?:,\d+)*\}").unwrap();
+    let button_regex = Regex::new(r"\(\d(?:,\d)*\)*").unwrap();
 
-        let result = regex.captures_iter(line);
-        let buttons: Vec<&str> = result
-            .map(|c| c.get_match().as_str())
-            .map(rem_first_and_last)
-            .collect();
+    let solution = pool.install(|| {
+        lines
+            .clone()
+            .par_bridge()
+            .progress_count(lines.count() as u64)
+            .map(|line| {
+                let joltage_requirements: Vec<i32> = joltage_regex
+                    .find(line)
+                    .map(|matches| matches.as_str())
+                    .map(remove_parentheses)
+                    .unwrap()
+                    .split(',')
+                    .flat_map(|c| c.parse().ok())
+                    .collect();
 
-        let max = joltage_requirements
-            .split(',')
-            .map(|d| d.parse().unwrap())
-            .max()
-            .unwrap();
-        // let mut cache = HashMap::new();
-        // let min_presses = bfs(HashMap::new(), 0, &buttons, joltage_requirements);
-        let min_presses = brute_force2(joltage_requirements, &buttons, max);
-        solution += min_presses.unwrap();
-    }
-    Some(solution as i32)
+                let buttons: Vec<Button> = button_regex
+                    .captures_iter(line)
+                    .map(|captures| captures.get_match().as_str())
+                    .map(remove_parentheses)
+                    .map(|button_str| {
+                        button_str
+                            .chars()
+                            .flat_map(|char| char.to_digit(10))
+                            .map(|digit| digit as usize)
+                            .collect()
+                    })
+                    .collect();
+                (buttons, joltage_requirements)
+            })
+            .map(|(buttons, joltage_requirements)| bfs(&buttons, &joltage_requirements))
+            .sum()
+    });
+    Some(solution)
 }
 
-fn bfs(
-    current: HashMap<char, i32>,
-    depth: i32,
-    buttons: &[&str],
-    joltage_requirements: &str,
-) -> Option<i32> {
-    let mut q = VecDeque::new();
-    q.push_back(current);
-    while !q.is_empty() {
-        let v = q.pop_front().unwrap();
-        if joltage_requirements.split(',').enumerate().all(|(i, c)| {
-            let indx = char::from_digit(i as u32, 10).unwrap();
-            let c_num = c.parse().unwrap();
-            v.get(&indx).unwrap_or(&0) == &c_num
-        }) {
-            return Some(depth);
+fn bfs(buttons: &[Button], joltage_requirements: &[i32]) -> i32 {
+    let mut queue = VecDeque::new();
+    let mut explored = HashMap::new();
+    explored.insert(joltage_requirements.to_vec(), joltage_requirements.to_vec());
+    queue.push_back(joltage_requirements.to_vec());
+    while !queue.is_empty() {
+        let current = queue.pop_front().unwrap();
+        if current.iter().all(|c| *c == 0) {
+            break;
         }
         for button in buttons {
-            let mut cloned = v.clone();
-            for c in button.chars() {
-                if let Some(n) = cloned.get(&c) {
-                    cloned.insert(c, n + 1);
-                } else {
-                    cloned.insert(c, 1);
+            let mut next = current.clone();
+            for index in button {
+                next[*index] -= 1;
+            }
+            if !explored.contains_key(&next) {
+                explored.insert(next.clone(), current.clone());
+                if next.iter().all(|joltage| *joltage >= 0) {
+                    queue.push_back(next);
                 }
             }
-            if !joltage_requirements.split(',').enumerate().any(|(i, c)| {
-                let indx = char::from_digit(i as u32, 10).unwrap();
-                let c_num = c.parse().unwrap();
-                cloned.get(&indx).unwrap_or(&0) > &c_num
-            }) {
-                q.push_back(cloned);
-            }
         }
     }
-    None
-}
+    let mut p = vec![0; joltage_requirements.len()];
 
-fn brute_force2(solution: &str, buttons: &[&str], presses: usize) -> Option<usize> {
-    if presses == 100 {
-        return None;
+    let mut steps = 0;
+    while p != joltage_requirements {
+        steps += 1;
+        p = explored.get(&p).unwrap().to_vec();
     }
-    dbg!(
-        buttons
-            .iter()
-            .combinations_with_replacement(presses)
-            .map(|combination| {
-                let mut v: Vec<String> = combination.iter().map(|s| s.to_string()).collect();
-                v.sort();
-                v
-            })
-            .unique()
-            .count()
-    );
-    dbg!(
-        buttons
-            .iter()
-            .combinations_with_replacement(presses)
-            .map(|combination| {
-                let mut v: Vec<String> = combination.iter().map(|s| s.to_string()).collect();
-                v.sort();
-                v
-            })
-            .count()
-    );
-    return None;
 
-    // if buttons
-    //     .iter()
-    //     .combinations_with_replacement(presses)
-    //     .map(|combination| {
-    //         let mut v: Vec<String> = combination.iter().map(|s| s.to_string()).collect();
-    //         v.sort();
-    //         v
-    //     })
-    //     .unique()
-    //     .par_bridge()
-    //     .any(|normilized| test_combination(normilized, solution))
-    // {
-    //     return Some(presses);
-    // }
-
-    // brute_force2(solution, buttons, presses + 1)
-}
-
-fn test_combination(
-    combination: Vec<String>,
-    joltage_requirements: &str,
-    // cache: &mut HashMap<Vec<String>, bool>,
-) -> bool {
-    // if let Some(b) = cache.get(&combination) {
-    //     return *b;
-    // }
-    let mut lights = HashMap::new();
-    for button in &combination {
-        for c in button.chars() {
-            if let Some(n) = lights.get(&c) {
-                lights.insert(c, n + 1);
-            } else {
-                lights.insert(c, 1);
-            }
-            let to_big = joltage_requirements.split(',').enumerate().any(|(i, c)| {
-                let indx = char::from_digit(i as u32, 10).unwrap();
-                let c_num = c.parse().unwrap();
-                lights.get(&indx).unwrap_or(&0) > &c_num
-            });
-            if to_big {
-                // cache.insert(combination, false);
-                return false;
-            }
-        }
-    }
-    let b = joltage_requirements.split(',').enumerate().all(|(i, c)| {
-        let indx = char::from_digit(i as u32, 10).unwrap();
-        let c_num = c.parse().unwrap();
-        lights.get(&indx).unwrap_or(&0) == &c_num
-    });
-    // cache.insert(combination, b);
-    b
+    steps
 }
 
 adventofcode::advent_of_code!(2025, 10, Some(7), Some(33));
